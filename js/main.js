8d61a7b80e5b125a4d86a67c110a7cf825712e3d
nelsonh88 <nelson.huezo88@gmail.com>
2019-03-10 17:36:04 -0400

    Works on a better version of form validation

--- js/main.js
+++ js/main.js
@@ -2,11 +2,15 @@
 
 console.log("aiiyo")
 
+
 const resetInputFields = function () {
   var frm = document.getElementsByName('userForm')[0];
   frm.reset();
 }
 
+let hasErrors = false
+
+/*
 const validation = function() {
   // let str = document.userForm.email.value
 
@@ -57,3 +61,84 @@ const validation = function() {
   resetInputFields()
   return (true);
 }
+*/
+
+// version 2 of validation
+const validation = function(e) {
+  const target = e.target;
+  const errmsg = target.parentNode.getElementsByClassName('errormessage');
+
+  if (target.value.length === 0) {
+    target.classList.add('error');
+      target.parentNode.classList.add('error');
+      errmsg[0].style.display = 'block';
+      return hasErrors = true
+    } else {
+	    errmsg[0].style.display = 'none';
+    	target.parentNode.classList.remove('error');
+    }
+  }
+
+  const emailValidation = function(e) {
+    const target = e.target;
+    const errmsg = target.parentNode.getElementsByClassName('errormessage');
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userForm.email.value)) {
+      target.classList.add('error');
+        target.parentNode.classList.add('error');
+        errmsg[0].style.display = 'block';
+        return hasErrors = true
+      } else {
+  	    errmsg[0].style.display = 'none';
+      	target.parentNode.classList.remove('error');
+      }
+  }
+
+  const phoneValidation = function(e) {
+    const target = e.target;
+    const errmsg = target.parentNode.getElementsByClassName('errormessage');
+    if (document.userForm.phone.value == "" || isNaN(document.userForm.phone.value) ||
+      document.userForm.phone.value.length != 10) {
+      target.classList.add('error');
+        target.parentNode.classList.add('error');
+        errmsg[0].style.display = 'block';
+        return hasErrors = true
+      } else {
+        errmsg[0].style.display = 'none';
+        target.parentNode.classList.remove('error');
+      }
+  }
+
+  const clientValidation = function(e) {
+    const target = e.target;
+    const errmsg = target.parentNode.getElementsByClassName('errormessage');
+    if (document.userForm.alreadyaclient.value == "") {
+      target.classList.add('error');
+        target.parentNode.classList.add('error');
+        errmsg[0].style.display = 'block';
+        return hasErrors = true
+      } else {
+        errmsg[0].style.display = 'none';
+        target.parentNode.classList.remove('error');
+      }
+  }
+
+  const checkAll = function() {
+    debugger
+//     let formFields = document.querySelectorAll('.formgroup');
+//     for (let i=0; i<formFields.length; i++) {
+//   	if (formFields[i].classList.contains('error')) {
+//       hasErrors = true;
+//       break;
+//     } else {
+//     	hasErrors = false;
+//       alert("Missing Fields");
+//     }
+//   }
+// }
+    if (hasErrors) {
+      alert("Form submitted successfully");
+      resetInputFields()
+    } else {
+      alert("Missing Fields");
+    }
+  }
